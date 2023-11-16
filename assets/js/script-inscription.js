404acb6c94f59f7f37d0353b392a0000f92bfa25
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2023-11-16 23:28:29 +0100

    verification mot de passe

--- assets/js/script-inscription.js
+++ assets/js/script-inscription.js
@@ -4,6 +4,7 @@ const keySecret = "$2a$10$HmGN6CC2dTwDcjw.3uGoGOBERN2BG/kqmmwIhiaB4mMnROa8wQlSy"
 const APIURL = "https://api.jsonbin.io/v3";
 const Bin = "6551f99512a5d3765998d7af";
 
+
 let objectDataForm;
 
 async function postBinContent() {
@@ -20,9 +21,24 @@ async function postBinContent() {
 let form = document.querySelector("form");
 form.addEventListener("submit", function (event){
     event.preventDefault();
+    verificationMotDePasse();
     let formData = new FormData(form);
     objectDataForm = Object.fromEntries(formData);
     console.log(objectDataForm);
-    postBinContent()
+    postBinContent();
+
     
-})
+    
+});
+
+function verificationMotDePasse() {
+     let motDepasse1 = document.getElementById("motDePasseInscription").value;
+     let motDepasse2 = document.getElementById("motDePasseBisInscription").value;
+            if (motDepasse1 === motDepasse2){
+                alert("ok!")
+            } else {
+                alert("vos mos de passe ne ont pas identiques")
+
+            }
+        
+    }
